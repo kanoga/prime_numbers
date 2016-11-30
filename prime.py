bceea15ddfee9f37a5052a4c445cf33fb2013eff
macnomore <ianmathenge@gmail.com>
2016-11-30 23:51:21 +0300

    Cannot allow list

--- prime.py
+++ prime.py
@@ -1,18 +1,25 @@
 def isPrime(number):
-    #check if number is float
+    	#check if number is float
     if isinstance(number,float):
         return "The number must be a whole number"
         #check if number is string
 	if isinstance(number, str):
 		return "Cannot allow letters"
 
+		#check if number is a list
+	if isinstance(number, list):
+		return "The input must not be a list"
+
         #check if number is tuple
     if isinstance(number,tuple):
         return "The input must not be a tuple"
+    
     if (number>1000):
         return "large numbers not allowed!!!"
+    
     if isinstance(number,bool):
         return "The number must not be a boolean"
+	
 	#getting the prime numbers between 0 and the number
 	a_list = [2]
 	for i in range(3,number+1):
