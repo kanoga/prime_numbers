0930584b97362c06edd4ac4a39dfc27501ad261b
Kimmirikwa <kimrodrikwa@gmail.com>
2016-11-30 19:14:19 +0300

    Add existing file

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,14 @@
 def isPrime(number):
     #handle the tests
-    if isinstance(number,int):
+    if not isinstance(number,int):
         return "The input must be a number only"
+    #getting the prime numbers between 0 and the number
+    a_list = [2]
+    for i in range(3, number + 1):
+    	prime = True
+    	for j in range(2, i):
+    		if i % j == 0:
+    			prime = False
+    	if prime:
+    		a_list.append(i)
+    return a_list
