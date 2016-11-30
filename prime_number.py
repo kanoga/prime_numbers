d0b2b4a5db979d751cc72d2d96c7ceb79b4ff80f
Kimmirikwa <kimrodrikwa@gmail.com>
2016-11-30 20:26:36 +0300

    Add existing file

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,13 @@
 def isPrime(number):
 	if isinstance(number,float):
 		return "The number must be a whole number"
-    #getting the prime numbers between 0 and the number
+	#getting the prime numbers between 0 and the number
+	a_list = [2]
+	for i in range(3,number+1):
+		prime = True
+		for j in range(2,1):
+			if i % j == 0:
+				print == False
+		if prime:
+			a_list.append(i)
+	return(a_list)
