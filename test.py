df7452c1e13391fbaa64f46e8ca87e7654a9b06f
Kimmirikwa <kimrodrikwa@gmail.com>
2016-11-30 19:44:54 +0300

    Add existing file

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ from prime_number import isPrime
 class PrimeTests(unittest.TestCase):
     def test_input_must_be_a_number(self):
         self.assertEqual(isPrime(1),"The input must be a number only")
-if __name__ == "__main__":
-    unittest.main()
     def test_for_zero_number(self):
         self.assertEqual(isPrime(0), "The input must be a positive non zero number")
 
