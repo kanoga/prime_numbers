f6b0f90e2052677e1e65cf02b959cf31ad2c1a52
Kimmirikwa <kimrodrikwa@gmail.com>
2016-11-30 20:12:01 +0300

    Add existing file

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import unittest
 from prime_number import isPrime
 class PrimeTests(unittest.TestCase):
-    def test_input_must_be_a_number(self):
-        self.assertEqual(isPrime(1),"The input must be a number only")
-    def test_for_zero_number(self):
-        self.assertEqual(isPrime(0), "The input must be a positive non zero number")
-
-    def test_for_not_boolean(self):
-        self.assertNotEqual(isPrime(0.5), "The input should be a whole number")
+	def test_input_must_not_be_float(self):
+		self.assertEqual(isPrime(10.5),"The number must be a whole number")
+if __name__ == "__main__":
+	unittest.main()
