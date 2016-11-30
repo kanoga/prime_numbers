7ec79e11f215bf01f6557b6cdd4633fb77af6420
Evans Wanjau <evanswanjau@gmail.com>
2016-11-30 17:59:02 +0300

    Add files via upload
    test input must not be a string

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import unittest
 from prime_number import isPrime
 class PrimeTests(unittest.TestCase):
     def test_input_must_be_a_number(self):
-        self.assertEqual(isPrime(1),"The input must be a number only")
+        self.assertEqual(isPrime(1)),"The input must be a number only")
+    def test_input_must_be_a_string(self):
+    	self.assertNotEqual("",(1)),"The input must not be a string")
 if __name__ == "__main__":
     unittest.main()
-    def test_for_zero_number(self):
-        self.assertEqual(isPrime(0), "The input must be a positive non zero number")
