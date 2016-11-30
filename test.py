fd89f5d4616164088b0fdd705572b41210207e7a
Kimmirikwa <kimrodrikwa@gmail.com>
2016-11-30 20:32:50 +0300

    Add existing file

--- test.py
+++ test.py
@@ -3,5 +3,3 @@ from prime_number import isPrime
 class PrimeTests(unittest.TestCase):
 	def test_input_must_not_be_float(self):
 		self.assertEqual(isPrime(10.5),"The number must be a whole number")
-if __name__ == "__main__":
-	unittest.main()
